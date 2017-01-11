18342f8294aa6a867d3e9efaa16eef9f9f673733
chang0022 <chang.0022@163.com>
2017-01-11 20:09:29 +0800

    添加 vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="app">
     <v-header></v-header>
-    <div class="tab">I am Tab</div>
+    <div class="tab">
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
+    </div>
     <div class="content">I am Content</div>
   </div>
 </template>
@@ -16,5 +20,13 @@
   };
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  .tab
+    display: flex
+    width: 100%
+    height 40px
+    line-height: 40px
+    .tab-item
+      flex: 1
+      text-align: center
 </style>
