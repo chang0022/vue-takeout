c0d5ccb2af72849af282b39390773ca893d94129
chang0022 <chang.0022@163.com>
2017-01-22 17:45:49 +0800

    滚动有问题 回家修改

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="(item, index) in goods" class="menu-item" :class="{'current': currentIndex===index}">
           <span class="text border-1px">
             <span v-show="item.type > 0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -22,12 +22,11 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span>月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span>￥{{food.price}}</span>
-                  <span v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show="food.oldPrice"
+                                                                class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,6 +38,8 @@
 </template>
 
 <script>
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
 
   export default {
@@ -49,8 +50,24 @@
     },
     data() {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
       };
+    },
+    computed: {
+      currentIndex() {
+        this.listHeight.forEach((v, i) => {
+          let height = v;
+          let nextHeight = this.listHeight[i + 1];
+          console.log(height);
+          console.log(nextHeight);
+          if (!nextHeight || (this.scrollY >= height && this.scrollY < nextHeight)) {
+            return i;
+          }
+        });
+        return 0;
+      }
     },
     created() {
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
@@ -60,8 +77,33 @@
           const DATA = response.data;
           if (DATA.errno === ERR_OK) {
             this.goods = DATA.data;
+            this.$nextTick(() => {
+              this._initScroll();
+              this._calculateHeight();
+            });
           }
         });
+    },
+    methods: {
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          probeType: 3
+        });
+
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+        });
+      },
+      _calculateHeight() {
+        let foodList = this.$refs.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        Array.prototype.forEach.call(foodList, (v) => {
+          height += v.clientHeight;
+          this.listHeight.push(height);
+        });
+      }
     }
   };
 </script>
@@ -87,6 +129,13 @@
         height 54px
         line-height 14px
         font-size 0
+        &.current
+          margin-top -1px
+          background-color #fff
+          font-weight 700
+          z-index 10
+          .text
+            border-none()
         .icon
           display inline-block
           vertical-align top
@@ -140,4 +189,25 @@
             line-height 14px
             font-size 14px
             color rgb(7, 17, 27)
+          .desc, .extra
+            font-size 10px
+            line-height 10px
+            color rgb(147, 153, 159)
+          .desc
+            margin-bottom 8px
+            line-height 12px
+          .extra
+            .count
+              margin-right 12px
+          .price
+            font-weight 700
+            line-height 24px
+            .now
+              margin-right 8px
+              font-size 14px
+              color rgb(240, 20, 20)
+            .old
+              font-size 10px
+              text-decoration line-through
+              color rgb(147, 153, 159)
 </style>
