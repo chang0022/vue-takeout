f49ab0dc3d377622f9abafe5db7e05fa05db57c3
chang0022 <chang.0022@163.com>
2017-02-15 23:38:18 +0800

    滚动动画失败 TODO

--- src/components/cartControl/cartControl.vue
+++ src/components/cartControl/cartControl.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="cartControl">
-    <div class="cart-decrease icon-remove_circle_outline" v-show="food.count > 0" @click="decreaseGoods"></div>
+    <transition name="move">
+      <div class="cart-decrease" v-show="food.count > 0" @click="decreaseGoods">
+        <transition name="inner">
+          <span class="inner icon-remove_circle_outline"></span>
+        </transition>
+      </div>
+    </transition>
     <div class="cart-count" v-show="food.count > 0">{{food.count}}</div>
     <div class="cart-add icon-add_circle" @click="addGoods"></div>
   </div>
@@ -41,12 +47,24 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .cartControl
     font-size 0
-    .cart-decrease, .cart-add
+    .cart-decrease
       display inline-block
       padding 6px
-      font-size 24px
-      line-height 24px
-      color rgb(0, 160, 220)
+      &.move-enter-active, &.move-leave-active
+        transition all .4s linear
+      &.move-enter, &.move-leave-active
+        opacity: 0
+        transform translate3d(24px, 0, 0)
+      .inner
+        display inline-block
+        font-size 24px
+        line-height 24px
+        color rgb(0, 160, 220)
+        &.inner-enter-active, &.inner-leave-active
+          transition: all .4s linear
+          transform rotate(0)
+        &.inner-enter, &.inner-leave-active
+          transform rotate(180deg)
     .cart-count
       display inline-block
       vertical-align top
@@ -56,4 +74,10 @@
       line-height 24px
       text-align center
       color rgb(147, 153, 159)
+    .cart-add
+      display inline-block
+      padding 6px
+      font-size 24px
+      line-height 24px
+      color rgb(0, 160, 220)
 </style>
